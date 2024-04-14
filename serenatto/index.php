aaff43d0f8181a44f1e89aed4b6f353fc7c685a8
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-04-14 17:45:51 -0300

    commit 3

--- serenatto/index.php
+++ serenatto/index.php
@@ -1,36 +1,12 @@
 <?php
 require "src/conexao.php";
 require "src/Modelo/Produto.php";
+require "src/Repositorio/ProdutoRepositorio.php";
 
-$sql1 = "SELECT * FROM produtos WHERE tipo='Café' ORDER BY preco";
-$statement = $pdo->query($sql1);
-$produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
+$produtoRepositorio = new ProdutoRepositorio($pdo);
+$dadosCafe = $produtoRepositorio->opcoesCafe();
+$dadosCafe = $produtoRepositorio->opcoesAlmoco();
 
-$dadosCafe = array_map(function ($cafe) {
-  return new Produto(
-    $cafe['id'],
-    $cafe['tipo'],
-    $cafe['nome'],
-    $cafe['descricao'],
-    $cafe['imagem'],
-    $cafe['preco'],
-  );
-}, $produtosCafe);
-
-$sql2 = "SELECT * FROM produtos WHERE tipo='Almoço' ORDER BY preco";
-$statement = $pdo->query($sql2);
-$produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-$dadosAlmoco = array_map(function ($almoco) {
-  return new Produto(
-    $almoco['id'],
-    $almoco['tipo'],
-    $almoco['nome'],
-    $almoco['descricao'],
-    $almoco['imagem'],
-    $almoco['preco'],
-  );
-}, $produtosAlmoco);
 ?>
 
 <!doctype html>
---
+++ serenatto/src/Repositorio/ProdutoRepositorio.php
@@ -0,0 +1,69 @@
+<?php
+
+class ProdutoRepositorio
+{
+  private PDO $pdo;
+
+  public function __construct(PDO $pdo)
+  {
+    $this->pdo = $pdo;
+  }
+
+  private function formarObjeto($dados)
+  {
+    return new Produto(
+      $dados['id'],
+      $dados['tipo'],
+      $dados['nome'],
+      $dados['descricao'],
+      $dados['imagem'],
+      $dados['preco']
+    );
+  }
+
+  public function opcoesCafe(): array
+  {
+    $sql = "SELECT * FROM produtos WHERE tipo='Café' ORDER BY preco";
+    $statement = $this->pdo->query($sql);
+    $produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $dadosCafe = array_map(function ($cafe) {
+      return $this->formarObjeto($cafe);
+    }, $produtosCafe);
+
+    return $dadosCafe;
+  }
+
+  public function opcoesAlmoco(): array
+  {
+    $sql = "SELECT * FROM produtos WHERE tipo='Almoço' ORDER BY preco";
+    $statement = $this->pdo->query($sql);
+    $produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $dadosAlmoco = array_map(function ($almoco) {
+      return $this->formarObjeto($almoco);
+    }, $produtosAlmoco);
+
+    return $dadosAlmoco;
+  }
+
+  public function buscarTodos(): array
+  {
+    $sql = "SELECT * FROM produtos ORDER BY preco";
+    $statement = $this->pdo->query($sql);
+    $dados = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $todosOsDados = array_map(function ($produto) {
+      return $this->formarObjeto($produto);
+    }, $dados);
+
+    return $todosOsDados;
+  }
+
+  public function deletar(int $id) {
+    $sql = "DELETE FROM produtos WHERE id = ?";
+    $statement = $this->pdo->prepare($sql);
+    $statement->bindValue(1, $id);
+    $statement->execute();
+  }
+}
